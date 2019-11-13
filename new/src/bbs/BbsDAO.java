0fe9d326f3a96236ee4c2ba6939c8f66d95e8482
cjedkfk <cjedkfk@gmail.com>
2019-11-13 21:57:40 +0900

    BbsDAO doxygen 작성

--- new/src/bbs/BbsDAO.java
+++ new/src/bbs/BbsDAO.java
@@ -14,6 +14,20 @@ public class BbsDAO {
     private Connection conn; // connection:db에 접근하게 해주는 객체
     private ResultSet rs;
 
+    /**
+	 *
+	 * @fn 		BbsDAO()
+	 * 
+	 * @brief 	DAO
+	 *
+	 * @author 	최지은
+	 * @date 	2019-11-13
+	 *
+	 *
+	 * @remark	DB 연결	[2019-11-13; 최지은] \n
+	 *
+	 */
+
     // mysql에 접속해 주는 부분
     public BbsDAO() { // 생성자 실행될때마다 자동으로 db연결이 이루어 질 수 있도록함
        try {
@@ -26,7 +40,20 @@ public class BbsDAO {
           e.printStackTrace(); // 오류가 무엇인지 출력
        }
     }
-    
+    /**
+	 *
+	 * @fn 		getDate()
+	 * 
+	 * @brief 	DAO
+	 *
+	 * @author 	최지은
+	 * @date 	2019-11-13
+	 *
+	 *
+	 * @remark	현재 날짜 입력	[2019-11-13; 최지은] \n
+	 *
+	 */
+
     public String getDate() {
     	String SQL = "SELECT NOW()";
     	try {
@@ -40,7 +67,19 @@ public class BbsDAO {
     	}
     	return ""; // DB 오류
     }
-    
+    /**
+	 *
+	 * @fn 		getNext()
+	 * 
+	 * @brief 	DAO
+	 *
+	 * @author 	최지은
+	 * @date 	2019-11-13
+	 *
+	 *
+	 * @remark	아이디 자동 +1	[2019-11-13; 최지은] \n
+	 *
+	 */
     public int getNext() {
     	String SQL = "SELECT bbsID FROM BBS ORDER BY bbsID DESC";
     	try {
@@ -55,7 +94,19 @@ public class BbsDAO {
     	}
     	return -1; // DB 오류
     }
-    
+    /**
+	 *
+	 * @fn 		write()
+	 * 
+	 * @brief 	DAO
+	 *
+	 * @author 	최지은
+	 * @date 	2019-11-13
+	 *
+	 *
+	 * @remark	글작성	[2019-11-13; 최지은] \n
+	 *
+	 */
     public int write(String bbsTitle, String userID, String bbsContent, String category, int groupId) {
     	String SQL = "INSERT INTO BBS VALUE (?, ?, ?, ?, ?, ?, ?, ?)";
     	try {
@@ -75,6 +126,19 @@ public class BbsDAO {
     	return -1; // DB 오류
     }
     
+    /**
+	 *
+	 * @fn 		getList()
+	 * 
+	 * @brief 	DAO
+	 *
+	 * @author 	최지은
+	 * @date 	2019-11-13
+	 *
+	 *
+	 * @remark	글 정보 호출	[2019-11-13; 최지은] \n
+	 *
+	 */
     public ArrayList<Bbs> getList(int pageNumber){
     	String SQL = "SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1 ORDER BY bbsID DESC LIMIT 10";
     	ArrayList<Bbs> list = new ArrayList<Bbs>();
@@ -99,7 +163,19 @@ public class BbsDAO {
     	}
     	return list; // DB 오류
     }
-    
+    /**
+	 *
+	 * @fn 		nextPage()
+	 * 
+	 * @brief 	DAO
+	 *
+	 * @author 	최지은
+	 * @date 	2019-11-13
+	 *
+	 *
+	 * @remark	글10개 이상 다음 페이지로 이동	[2019-11-13; 최지은] \n
+	 *
+	 */
     public boolean nextPage(int pageNumber) {
     	String SQL = "SELECT * FROM BBS WHERE bbsID < ? AND bbsAvailable = 1 ";
     	try {
@@ -114,7 +190,19 @@ public class BbsDAO {
     	}
     	return false; // DB 오류
     }
-    
+    /**
+	 *
+	 * @fn 		getBbs()
+	 * 
+	 * @brief 	DAO
+	 *
+	 * @author 	최지은
+	 * @date 	2019-11-13
+	 *
+	 *
+	 * @remark	글 1건 호출	[2019-11-13; 최지은] \n
+	 *
+	 */
     public Bbs getBbs(int bbsID) {
     	String SQL = "SELECT * FROM BBS WHERE bbsID = ?";
     	try {
@@ -138,7 +226,19 @@ public class BbsDAO {
     	}
     	return null; // DB 오류
     }
-    
+    /**
+	 *
+	 * @fn 		update()
+	 * 
+	 * @brief 	DAO
+	 *
+	 * @author 	최지은
+	 * @date 	2019-11-13
+	 *
+	 *
+	 * @remark	글 1건 수정	[2019-11-13; 최지은] \n
+	 *
+	 */
     public int update(int bbsID, String bbsTitle, String bbsContent) {
     	String SQL = "UPDATE BBS SET bbsTitle = ?, bbsContent = ? WHERE bbsID = ?";
     	try {
@@ -152,6 +252,19 @@ public class BbsDAO {
     	}
     	return -1; // DB 오류ㄴ
     }
+    /**
+	 *
+	 * @fn 		delete()
+	 * 
+	 * @brief 	DAO
+	 *
+	 * @author 	최지은
+	 * @date 	2019-11-13
+	 *
+	 *
+	 * @remark	글 1건 삭제	[2019-11-13; 최지은] \n
+	 *
+	 */
     public int delete(int bbsID) {
     	String SQL = "UPDATE BBS SET bbsAvailable = 0 WHERE bbsID = ?";
     	try {
@@ -163,6 +276,19 @@ public class BbsDAO {
     	}
     	return -1; // DB 오류
     }
+    /**
+	 *
+	 * @fn 		getNewGroupId()
+	 * 
+	 * @brief 	DAO
+	 *
+	 * @author 	최지은
+	 * @date 	2019-11-13
+	 *
+	 *
+	 * @remark	새로운 GroupId 생성	[2019-11-13; 최지은] \n
+	 *
+	 */
     public int getNewGroupId() {
     	
     	int newGroupId = 0;
